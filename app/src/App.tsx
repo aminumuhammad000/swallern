a7e5acf2f756db4324c35168f793a15896e73804
aminumuhammad000 <aminumuhammad00015@gmail.com>
2025-08-19 18:29:57 +0100

    setting the swallern in my linux

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,6 +2,7 @@ const App = () => {
   return (
     <div>
       <h1>Hello User Welcome to Swallern</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero odio architecto quo numquam, mollitia, voluptates velit accusamus non sapiente laborum dolor nobis sint officiis molestias, neque voluptas autem laboriosam deserunt!</p>
     </div>
   );
 };
