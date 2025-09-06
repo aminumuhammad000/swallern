9367acc0ed3404b1b58b6201dba271135ddbfa5c
aminumuhammad000 <aminumuhammad00015@gmail.com>
2025-09-06 01:24:26 +0100

    created the hero section in lanfing page

--- app/src/Pages/home/layout/Hero.tsx
+++ app/src/Pages/home/layout/Hero.tsx
@@ -1,13 +1,69 @@
 import style from "./Hero.module.css";
+import user from "../../../assets/images/user.png";
+import user2 from "../../../assets/images/user2.png";
+import StarIcon from "@mui/icons-material/Star";
+
+// import { use } from "react";
 
 export const Hero = () => {
   return (
     <div className={style.Hero}>
       <div>
-        <h1>Learn Fast. Grow Wise. Build What Matters</h1>
+        <h1 className={style.title}>
+          Learn Fast. Grow Wise.{" "}
+          <span className={style.color}>Build What Matters</span>
+        </h1>
+        <p className={style.description}>
+          Swallern is an empowering platform for young creators to gain
+          real-world skills, collaborate with teams, and turn learning into
+          action. with fun, purpose, and impact.
+        </p>
+        <div className={style.buttons}>
+          <button className={style.primary}>Start Your Learning Journey</button>
+          <button className={style.secondary}>Explore Swallern</button>
+        </div>
+
+        <div className={style.testimonials}>
+          <div className={style.imagesContainer}>
+            <img src={user2} alt="user profile" className={style.userImage} />
+            <img src={user2} alt="user profile" className={style.userImage} />
+            <img src={user2} alt="user profile" className={style.userImage} />
+            <img src={user2} alt="user profile" className={style.userImage} />
+            <img src={user2} alt="user profile" className={style.userImage} />
+            <img src={user2} alt="user profile" className={style.userImage} />
+            <button className={style.add}>+</button>
+          </div>
+          <div>
+            <p>
+              <div className={style.starContainer}>
+                <StarIcon fontSize="small" style={{ color: "gold" }} />
+                <StarIcon fontSize="small" style={{ color: "gold" }} />
+                <StarIcon fontSize="small" style={{ color: "gold" }} />
+                <StarIcon fontSize="small" style={{ color: "gold" }} />
+                <StarIcon fontSize="small" style={{ color: "gold" }} />
+                {/* <StarIcon fontSize="small" style={{ color: "gold" }} /> */}
+                <span> (4.7)</span>
+              </div>
+            </p>
+            <p className={style.reviewText}>200+ Reviews on our courses</p>
+          </div>
+        </div>
       </div>
 
-      <div></div>
+      <div className={style.circleContainer}>
+        <div className={style.circle}>
+          <img src={user} alt="user image" className={style.image} />
+        </div>
+        <div className={style.courseBox}>
+          <h2 className={style.courseNumber}>5+</h2>
+          <p>Digital Courses</p>
+        </div>
+
+        <div className={style.courseCard}>
+          <h3>Web Design Beginner Course</h3>
+          <button className={style.join}>Join Now</button>
+        </div>
+      </div>
     </div>
   );
 };
--- app/src/Pages/home/layout/Nav.tsx
+++ app/src/Pages/home/layout/Nav.tsx
@@ -1,17 +1,19 @@
-import style from './Nav.module.css';
-import logo from '../../../assets/logo.png'; // Assuming you have a logo image in this path
+import style from "./Nav.module.css";
+import logo from "../../../assets/logo.png"; // Assuming you have a logo image in this path
 export const Nav = () => {
   return (
-    <div className={style.Nav} id='flexSpaceBetween'>
-        <img src={logo} alt="logo of swallern" className={style.logo}/>
-        <ul id='flexSpaceBetween' className={style.navLinks}>
-            <li id='mediumtext' className={style.active}>Home</li>
-            <li id='mediumtext'>About us</li>
-            <li id='mediumtext'>Contact us</li>
-            <li id='mediumtext'>FAQs</li>
-        </ul>
+    <div className={style.Nav} id="flexSpaceBetween">
+      <img src={logo} alt="logo of swallern" className={style.logo} />
+      <ul id="flexSpaceBetween" className={style.navLinks}>
+        <li id="mediumtext" className={style.active}>
+          Home
+        </li>
+        <li id="mediumtext">About us</li>
+        <li id="mediumtext">Contact us</li>
+        <li id="mediumtext">FAQs</li>
+      </ul>
 
-        <button className={style.getStarted}>Get Started Free</button>
+      <button className={style.getStarted}>Get Started Free</button>
     </div>
-  )
-}
+  );
+};
